0ff78a9f9737b7faf0263cf407e18bd20dc8c8c3
Kirtanshah2303 <shahkirtanv2002@gmail.com>
2023-11-17 00:06:17 +0530

    payment added

--- src/components/OngoingDealProperty.js
+++ src/components/OngoingDealProperty.js
@@ -8,9 +8,27 @@ const OngoingDealProperty = () => {
   const navigate = useNavigate();
   const [rowData, setRowData] = useState([]);
   const token = getToken();
+  const [paySuccess, setPaySuccess] = useState(false);
 
   const handleContractClick = (event, param) => {
     navigate('/view/sellerContract/' + param);
+  };
+
+  const loadScript = (src) => {
+    return new Promise((resovle) => {
+      const script = document.createElement("script");
+      script.src = src;
+
+      script.onload = () => {
+        resovle(true);
+      };
+
+      script.onerror = () => {
+        resovle(false);
+      };
+
+      document.body.appendChild(script);
+    });
   };
 
   const fetchData = async () => {
@@ -32,6 +50,74 @@ const OngoingDealProperty = () => {
         console.error("Error fetching data:", error);
     }
 };
+
+const handlePayClick = async (id) => {
+  // Add your payment logic here, e.g., navigate to a payment page
+  
+  console.log("Handle patment called!");
+  const res = await loadScript(
+    "https://checkout.razorpay.com/v1/checkout.js"
+  );
+  if (!res) {
+    alert("You are offline... Failed to load Razorpay SDK");
+    return;
+  }
+
+  const token = getToken();
+  fetch('http://192.168.2.251:8080/api/paySellAmount/'+ id,{
+      headers:{
+          "Authorization" : "Bearer " + token
+      }
+  })
+    .then(response => response.json())
+    .then(paymentDetails => {
+      const options = {
+        key: 'rzp_test_fE9CO5AP0eMCxf', // Replace with your Razorpay API key
+        secret: 'qoNEOQjaWXud6VPPqOtgGgIH',
+        amount: paymentDetails.propertyRental.rentAmount * 100, // Amount is in paisa
+        currency: 'INR', // Change currency as needed,
+        // callback_url: 'http://192.168.2.251:8080/api/payment/success/'+paymentDetails.orderId,
+      //   name: 'Your Company Name',
+      //   description: 'Rent Payment',
+      //   image: 'URL_TO_YOUR_LOGO',
+        prefill: {
+          name: 'Devkul',
+          email: 'test@example.com', // Replace with the email of the payer
+        },
+        notes: {
+          rentalPaidDate: '11/11/2023',
+          paidBy: 'Devkul',
+          paidTo: paymentDetails.propertyRental.paidTo,
+        },
+        theme: {
+          color: '#3498db', // Change color as needed
+        },
+        handler: function (response) {
+          // Handle success
+          setPaySuccess(true);
+          alert(response.razorpay_payment_id);
+          axios.post('http://192.168.2.251:8080/api/payment/success/sell/'+paymentDetails.orderId+'?razorpay_payment_id='+response.razorpay_payment_id,{},{
+            headers:{
+              'Authorization' : "Bearer "+token
+            }
+          }).then(() => {
+            alert("Payment successful");
+          }).catch((e)=>{
+            alert(e);
+          })
+          
+          console.log(response);
+        }
+      };
+
+      const rzp = new window.Razorpay(options);
+      rzp.open();
+    })
+    .catch(error => console.error('Error fetching payment details:', error));
+
+  // navigate('/payment');
+};
+
 
   useEffect(() => {
     // Define an async function to fetch data
@@ -61,6 +147,7 @@ if (rowData.length === 0) {
               <th>Sale Amount</th>
               <th>Deal Done Date</th>
               <th>Contract</th>
+              <th>Payment</th>
             </tr>
           </thead>
           <tbody>
@@ -83,6 +170,11 @@ if (rowData.length === 0) {
                         >Contract
                         </button>
                     </td>
+                    <td>
+                        <button className="btn btn-primary" type='button' onClick={() => handlePayClick(data.id)}>
+                          Pay
+                        </button>
+                      </td>
                     {/* Add more table data cells for other properties */}
                 </tr>
             ))}
