24b6f67a8edd55b1e6b5401a77a5edec0c6bd725
Kirtanshah2303 <shahkirtanv2002@gmail.com>
2023-11-16 18:33:22 +0530

    pay button logic modified

--- src/components/ViewMyRentedProperty.js
+++ src/components/ViewMyRentedProperty.js
@@ -7,6 +7,7 @@ const ViewMyRentedProperty = () => {
   const navigate = useNavigate();
   const token = getToken();
   const [rowData, setRowData] = useState([]);
+  const [paySuccess, setPaySuccess] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,15 +26,86 @@ const ViewMyRentedProperty = () => {
     fetchData();
   }, [token]);
 
-  const handlePayClick = () => {
+  const loadScript = (src) => {
+    return new Promise((resovle) => {
+      const script = document.createElement("script");
+      script.src = src;
+
+      script.onload = () => {
+        resovle(true);
+      };
+
+      script.onerror = () => {
+        resovle(false);
+      };
+
+      document.body.appendChild(script);
+    });
+  };
+
+
+  const handlePayClick = async (id) => {
     // Add your payment logic here, e.g., navigate to a payment page
-    navigate('/payment');
+    const res = await loadScript(
+      "https://checkout.razorpay.com/v1/checkout.js"
+    );
+    if (!res) {
+      alert("You are offline... Failed to load Razorpay SDK");
+      return;
+    }
+
+    const token = getToken();
+    fetch('http://192.168.2.251:8080/api/payRent/'+ id,{
+        headers:{
+            "Authorization" : "Bearer " + token
+        }
+    })
+      .then(response => response.json())
+      .then(paymentDetails => {
+        const options = {
+          key: 'rzp_test_fE9CO5AP0eMCxf', // Replace with your Razorpay API key
+          secret: 'qoNEOQjaWXud6VPPqOtgGgIH',
+          amount: paymentDetails.propertyRental.rentAmount * 100, // Amount is in paisa
+          currency: 'INR', // Change currency as needed
+        //   name: 'Your Company Name',
+        //   description: 'Rent Payment',
+        //   image: 'URL_TO_YOUR_LOGO',
+          prefill: {
+            name: 'Devkul',
+            email: 'test@example.com', // Replace with the email of the payer
+          },
+          notes: {
+            rentalPaidDate: '11/11/2023',
+            paidBy: 'Devkul',
+            paidTo: paymentDetails.propertyRental.paidTo,
+          },
+          theme: {
+            color: '#3498db', // Change color as needed
+          },
+          handler: function (response) {
+            // Handle success
+            setPaySuccess(true);
+            alert(response.razorpay_payment_id);
+            alert("Payment successful");
+            console.log(response);
+          }
+        };
+
+        const rzp = new window.Razorpay(options);
+        rzp.open();
+      })
+      .catch(error => console.error('Error fetching payment details:', error));
+
+    // navigate('/payment');
   };
 
   return (
     <div className="saleContainer">
       <div className="header">
         <h2>My rented property View Page Details</h2>
+      </div>
+      <div className={`alert alert-success ${paySuccess ? '' : 'd-none'}`} id="pay-success">
+        <strong>Payment Completed Successfully</strong>
       </div>
 
       <div className="row">
@@ -76,7 +148,7 @@ const ViewMyRentedProperty = () => {
                       <td>{data.typeOfProperty}</td>
                       <td>{data.lesseeName}</td>
                       <td>
-                        <button className="btn btn-primary" onClick={handlePayClick}>
+                        <button className="btn btn-primary" onClick={handlePayClick(data.id)}>
                           Pay
                         </button>
                       </td>
